6fe1d75e775d308f78ee8aee68cc7c60fbac919a
dogura <daiki.ogura@soliton.co.jp>
2013-11-12 20:48:40 +0900

    テキストメッセージのデコードまでを実装できた

--- Main.hx
+++ Main.hx
@@ -7,7 +7,9 @@ import neko.Lib;
 import neko.net.ThreadServer;
 import neko.net.ServerLoop;
 import haxe.ds.Option;
+import haxe.ds.Vector;
 import haxe.io.Bytes;
+import haxe.io.BytesInput;
 import neko.vm.Deque;
 
 
@@ -26,11 +28,6 @@ class Main {
         webs.run("localhost", 1234);
         trace("server halt");
     }
-}
-
-
-class WebSocketProcol extends haxe.remoting.SocketProtocol {
-
 }
 
 
@@ -56,7 +53,7 @@ class WebSocket {
 
     /**
       * @param sec_websocket_key
-      * 
+      *
      */
     public static function get_hand_shake(sec_websocket_key: String): String
     {
@@ -87,7 +84,7 @@ typedef Message = {
 class WebSocketServer extends ThreadServer<Client, Message> {
 
     /**
-      * 
+      *
         1 GET /chat HTTP/1.1
         2 Host: server.example.com
         3 Upgrade: websocket
@@ -119,6 +116,8 @@ class WebSocketServer extends ThreadServer<Client, Message> {
             this.hand_shake(c, msg);
             return {msg: {str: msg}, bytes: len};
         }
+        var content = decode_message(buf, pos, len);
+        trace(content);
         trace('============================== get end\n');
         return {msg: {str: msg}, bytes: len};
     }
@@ -158,6 +157,85 @@ class WebSocketServer extends ThreadServer<Client, Message> {
         }
 
     }
+
+    /**
+      * 
+      * via: http://d.hatena.ne.jp/gtk2k/20120203/1328275041
+     */
+    function decode_message(buf: Bytes, pos: Int, len: Int): String {
+        var datas = new BytesInput(buf, pos, len);
+        var fin_rsv_opcode = datas.readByte();
+        // 10000000 & 10000000 == 10000000  一番左端のビットが立っているかどうかの計算
+        var fin: Bool = (fin_rsv_opcode & 0x80) == 0x80;
+        var opcode = fin_rsv_opcode & 0x0f;
+        if (opcode == OPCODE.Text) {
+            var data_header = datas.readByte();
+            var is_masked: Bool = (data_header & 0x80) == 0x80;
+            if (!is_masked) { throw "Client should mask datas."; }
+            var decode_payload_length =
+                function(len_of_byte: Int) {
+                    var _payload = datas.read(len_of_byte).toHex();
+                    trace('payload data = ${_payload}');
+                    return Std.parseInt(_payload);
+                }
+            var payload_length = switch (data_header & 0x7f) {
+                                  case 126: decode_payload_length(2);
+                                  case 127: decode_payload_length(2);
+                                  case i: i;
+                              }
+            var masking_key: Vector<Int> = new Vector(4);
+                [for (i in 0...4) masking_key.set(i, datas.readByte())];
+            // var payload_datas = datas.read(payload_length);
+            var decoded_payload_data = [
+                for (i in 0...payload_length)
+                    // payload_datas.get(i) ^ masking_key[i % 4]
+                    datas.readByte() ^ masking_key[i % 4]
+                ];
+            var result = new StringBuf();
+            decoded_payload_data.iter(function(c) { result.addChar(c); });
+            return result.toString();
+            // return decoded_payload_data
+            //     .map(
+            //         function(_byte) {
+            //             trace('decoded payload = ${_byte}');
+            //             return String.fromCharCode(_byte);
+            //         })
+            //     .fold(
+            //         function(_ch1, ch2) { return _ch1 + ch2; },
+            //         "")
+            //     ;
+        } else {
+            throw "NotImplemented 工事中";
+        }
+    }
 }
 
+
+/**
+  * 
+  * - opcodeについて
+  * %x0 は継続フレームを表す
+  * %x1 はテキストフレームを表す
+  * %x2 はバイナリフレームを表す
+  * %x3-7 は追加の非制御フレーム用に予約済み
+  * %x8 は接続の切断を表す
+  * %x9 は ping を表す
+  * %xA は pong を表す
+  * %xB-F は追加の制御フレーム用に予約済み
+ */
+class OPCODE {
+    public static inline var Continuation = 0x00;
+    public static inline var Text = 0x01;
+    public static inline var Binary = 0x02;
+    public static inline var Close = 0x08;
+    public static inline var Ping = 0x09;
+    public static inline var Pong = 0x0A;
+
+    // public static inline var RECEIVED1 = 0x03;
+    // public static inline var RECEIVED2 = 0x04;
+    // public static inline var RECEIVED3 = 0x05;
+    // public static inline var RECEIVED4 = 0x06;
+    // public static inline var RECEIVED5 = 0x07;
+    // B-Fも予約済み
+}
 
